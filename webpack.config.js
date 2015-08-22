0e7c9f14b2327300d580db39477bfc21dbbef68c
Vadim Sikora <vadim.sikora@divio.ch>
2015-08-22 18:51:24 +0200

    tooling

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devtool: 'cheap-module-eval-source-map',
+    entry: [
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server',
+        './index'
+    ],
+    output: {
+        path: __dirname,
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
+    resolve: {
+        extensions: ['', '.js']
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loaders: ['react-hot', 'babel'],
+            exclude: /node_modules/,
+            include: __dirname
+        }]
+    }
+};
---
+++ webpack.config.production.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var webpack = require('webpack');
+var _ = require('lodash');
+var baseConfig = require('./webpack.config');
+
+var config = _.assign({}, baseConfig, {
+    devtool: false,
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin(),
+        // use react production build (much smaller)
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
+    ],
+    entry: './index',
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel',
+            exclude: /node_modules/,
+            query: {
+                optional: ['runtime']
+            }
+        }]
+    }
+});
+
+module.exports = config;
