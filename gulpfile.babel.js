083d13aad8a4fa61c4c887348d7d25d742ba6a2a
Vadim Sikora <vadim.sikora@divio.ch>
2015-09-06 21:21:44 +0200

    icons

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,8 @@
 import gulp from 'gulp';
 import gutil from 'gulp-util';
 import sass from 'gulp-sass';
+import iconfont from 'gulp-iconfont';
+import iconfontCss from 'gulp-iconfont-css';
 import autoprefixer from 'gulp-autoprefixer'
 import webpack from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
@@ -12,6 +14,23 @@ gulp.task('sass', () =>
         .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(autoprefixer())
         .pipe(gulp.dest('./')));
+
+gulp.task('icons', () =>
+    gulp.src('fonts/src/*.svg')
+        .pipe(iconfontCss({
+            fontName: 'iconfont',
+            fontPath: '../fonts/',
+            path: 'sass/libs/_iconfont.scss',
+            targetPath: '../sass/components/_iconography.scss'
+        }))
+        .pipe(iconfont({
+            fontName: 'iconfont',
+            normalize: true
+        }))
+        .on('glyphs', function (glyphs, options) {
+            gutil.log.bind(glyphs, options);
+        })
+        .pipe(gulp.dest('fonts/')));
 
 gulp.task('webpack:server', () => {
     new WebpackDevServer(webpack(devConfig), {
