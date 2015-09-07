88be78d82f4891850d7bca2284df7194d4eb9531
Vadim Sikora <vadim.sikora@divio.ch>
2015-09-07 10:21:31 +0200

    fix the link to home page

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ export const ORG_NAME = 'divio';
 export const INVALIDATE_TIME = 3600 * 1000;
 
 export const LINKS = {
-    HOME: 'http://www.divio.ch',
+    HOME: 'http://www.divio.com',
     FOOTER: {
         'About us': 'http://www.divio.com/en/about-us/',
         'Contact': 'http://www.divio.com/en/contact/',
